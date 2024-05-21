44108b4b52c0d21d50ab6cea9854c7890565b4cc
I514807 <nitin.kumar15@sap.com>
2024-05-21 15:42:27 +0530

    feat: initial commit

---
+++ src/main/java/com/generalnitin/wiremockstubserver/WiremockStubServerApplication.java
@@ -0,0 +1,13 @@
+package com.generalnitin.wiremockstubserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiremockStubServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiremockStubServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/generalnitin/wiremockstubserver/WiremockStubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generalnitin.wiremockstubserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiremockStubServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
